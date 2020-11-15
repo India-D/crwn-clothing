8cbb7d91903470dd263d6066eb839f211ec629c7
India <dabon.india@gmail.com>
2020-11-15 04:30:07 +0100

    added files for homepage and broken homepage into smaller component, finished styling our menu-item component

---
+++ src/pages/Homepage/homePage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../components/Directory/Directory.component";
+import "./homePage.style.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
